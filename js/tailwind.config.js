8a8331987339ba194983109c3eceb13e508370b3
Jakub0821 <jakubmigda120@gmail.com>
2024-08-10 00:53:31 +0200

    change files

--- js/tailwind.config.js
+++ js/tailwind.config.js
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './**/*.html',    // Dopasowuje wszystkie pliki HTML w głównym katalogu oraz w podkatalogach
+    './js/**/*.js',   // Dopasowuje wszystkie pliki JS w katalogu 'js' oraz jego podkatalogach
+    './css/**/*.css', // Dopasowuje wszystkie pliki CSS w katalogu 'css' oraz jego podkatalogach
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#4CAF50',  // Zielony kolor używany w nagłówkach i przyciskach
+        secondary: '#333333',  // Ciemnoszary kolor używany w nawigacji
+        accent: '#FFFFFF',    // Biały kolor używany w tekście nagłówków i nawigacji
+        dark: '#000000',      // Czarny kolor, który może być używany w stopce
+        light: '#f4f4f4',     // Jasnoszary kolor tła używany na stronie
+      },
+    },
   },
   plugins: [],
 }
-
