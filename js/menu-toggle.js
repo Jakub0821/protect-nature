850d658f9d36f25db440af8c36c5671f7376fc54
Jakub0821 <jakubmigda120@gmail.com>
2024-08-13 18:41:27 +0200

    change files

--- js/menu-toggle.js
+++ js/menu-toggle.js
@@ -1,5 +1,6 @@
-// menu-toggle.js
-document.getElementById('tips').addEventListener('click', function(e) {
-    e.preventDefault();
-    document.getElementById('ecology-submenu').classList.toggle('hidden');
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('tips').addEventListener('click', function(e) {
+        e.preventDefault();
+        document.getElementById('ecology-submenu').classList.toggle('hidden');
+    });
 });
