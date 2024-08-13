beef7e9bb2a8eddd4faf7f4f77721ce7e00c9bdd
Jakub0821 <jakubmigda120@gmail.com>
2024-08-13 20:37:15 +0200

    change

--- js/menu-toggle.js
+++ js/menu-toggle.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('tips').addEventListener('click', function(e) {
-        document.getElementById('ecology-submenu').classList.toggle('hidden');
+        document.getElementById('relative group').classList.toggle('hidden');
     });
 });
